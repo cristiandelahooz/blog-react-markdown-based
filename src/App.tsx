f20d40ab061e938792afd130fd958becf17651ac
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-04 15:59:11 -0400

    refactor(css): remove unnecessary styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { create } from 'zustand'
-import viteLogo from '/vite.svg'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -14,30 +13,19 @@ const userCounterSore = create<CounterState>(set => ({
       count: ++state.count
     }))
 }))
-function App() {
+export default function App() {
   const { count, increment } = userCounterSore()
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://github.com/cristiandelahooz" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="animation-h1">Sakidoa</h1>
       <div className="card">
         <button onClick={increment}>count is {count}!!!</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
