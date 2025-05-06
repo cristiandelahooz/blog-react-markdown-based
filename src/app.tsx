d920a149f83a83f326a51f42b6685d99939ab964
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-05 22:58:26 -0400

    feat(app): add page-title component

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import { Container } from '@/atoms/container'
 import { MainContent } from '@/components/main-content'
 import { MarkdownView } from '@/components/markdown-view'
 import './app.css'
+import { PageTitle } from './components/page-title'
 
 export default function App() {
   return (
     <Container>
       <MainContent>
+        <PageTitle />
         <MarkdownView />
       </MainContent>
     </Container>
