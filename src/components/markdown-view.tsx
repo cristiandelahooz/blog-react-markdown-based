086cca799cfdfd9cbf78fe2f4405b7a265ac5372
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-06 08:19:23 -0400

    feat(markdown-view): add style

--- src/components/markdown-view.tsx
+++ src/components/markdown-view.tsx
@@ -2,6 +2,7 @@ import markdownContent from '@/example.md?raw'
 import { useContentStore } from '@/stores/content'
 import { Box } from '@kuma-ui/core'
 import React, { useEffect } from 'react'
+import './markdown-view.css'
 
 export const MarkdownView: React.FC = () => {
   const { dom, render } = useContentStore()
