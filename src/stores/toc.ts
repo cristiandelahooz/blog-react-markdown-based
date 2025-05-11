98e7409f96d933dd1462b2e74057923bf089f41e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-10 21:08:18 -0400

    feat(content): add `registerHeading`

--- src/stores/toc.ts
+++ src/stores/toc.ts
@@ -5,11 +5,15 @@ import {
 import type { Root } from 'mdast'
 import { create } from 'zustand'
 import { loggerMiddleware } from './logger'
+import { RefObject } from 'react'
 
-export type Section = TOCHeading
+export type Section = TOCHeading & {
+  headingRef: RefObject<HTMLHeadingElement> | null
+}
 export type TocState = {
   sections: Section[]
   update: (mdast: Root) => void
+  registerHeading: (id: string, ref: RefObject<HTMLHeadingElement>) => void
 }
 
 export const useTocStore = create<TocState>(
@@ -17,11 +21,21 @@ export const useTocStore = create<TocState>(
     sections: [],
     update: (mdast: Root) => {
       if (mdast) {
-        const sections = mdastExtractHeadings(mdast)
+        const sections = mdastExtractHeadings(mdast).map(h => ({
+          ...h,
+          headingRef: null
+        }))
         set({ sections })
       } else {
         set({ sections: [] })
       }
+    },
+    registerHeading: (id: string, ref: RefObject<HTMLHeadingElement>) => {
+      set(state => ({
+        sections: state.sections.map(s =>
+          s.id === id ? { ...s, headingRef: ref } : s
+        )
+      }))
     }
   }))
 )
