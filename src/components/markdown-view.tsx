6eb1387198a1157c3ed9dc67f175b0439b6507b9
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-11 20:28:46 -0400

    feat(`markdown-view.tsx`): use check active sections hook

--- src/components/markdown-view.tsx
+++ src/components/markdown-view.tsx
@@ -3,11 +3,13 @@ import { useContentStore } from '@/stores/content'
 import { Box } from '@kuma-ui/core'
 import React, { useEffect } from 'react'
 import './markdown-view.css'
+import { useVisibleSections } from '@/hooks/use-visible-sections'
 
 export const MarkdownView: React.FC = () => {
   const { dom, render } = useContentStore()
   useEffect(() => {
     render(markdownContent)
   }, [render])
+  useVisibleSections()
   return <Box className="markdown-view">{dom || 'rendering...'}</Box>
 }
